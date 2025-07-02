ce7374a89d987b1638e0d4515d0202e894756d10
Anikavuk <usla_2018@mail.ru>
2025-07-02 12:04:17 +0500

    fix: main.py

--- app/main.py
+++ app/main.py
@@ -9,8 +9,6 @@ def read_custom_message():
     return {"message": "This is a custom message!"}
 
 
-#
 @app.get("/")
 def read_root():
-    # logger.info("Handling request to root endpoint")
-    return {"message": "Hello"}
+    return {"message": "Hello world!"}
